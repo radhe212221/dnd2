1c274ac5bc8f620726490bd7f5d71dd563543745
RS <radhe212221@gmail.com>
2022-06-15 21:49:37 +0530

    added team img

--- src/comp/team.js
+++ src/comp/team.js
@@ -2,10 +2,10 @@ import React from 'react'
 
 export default function Team() {
   const a = [
-    { h2: "demo1", h3: "frontend", h4: "html5", a: "#" },
-    { h2: "demo2", h3: "frontend", h4: "html5", a: "#" },
-    { h2: "demo3", h3: "frontend", h4: "html5", a: "#" },
-    { h2: "demo4", h3: "frontend", h4: "html5", a: "#" },
+    { src: "https://www.w3schools.com/w3images/team1.jpg", h2: "demo1", h3: "frontend", h4: "html5", a: "#" },
+    { src: "https://www.w3schools.com/w3images/team2.jpg", h2: "demo2", h3: "frontend", h4: "html5", a: "#" },
+    { src: "https://www.w3schools.com/w3images/team3.jpg", h2: "demo3", h3: "frontend", h4: "html5", a: "#" },
+    { src: "https://www.w3schools.com/w3images/team4.jpg", h2: "demo4", h3: "frontend", h4: "html5", a: "#" },
   ]
   return (
     <div className='team'>
@@ -13,6 +13,7 @@ export default function Team() {
       <p>we re group industry experts</p>
       <div className='flex'>
         {a.map(x => <div className='item'>
+          <img src={x.src} />
           <h2>{x.h2}</h2>
           <h3>{x.h3}</h3>
           <h4>{x.h4}</h4>
