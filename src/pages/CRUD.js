8da1fa30807479b95e538f291f70d0725272ac53
RS <radhe212221@gmail.com>
2022-06-15 23:38:22 +0530

    crud added

--- src/pages/CRUD.js
+++ src/pages/CRUD.js
@@ -1,9 +1,13 @@
-import React, { useState } from 'react'
+import axios from 'axios'
+import React, { useRef, useState } from 'react'
+import { useSelector } from 'react-redux'
 
 const TEAM = { src: "", h2: "", h3: "", h4: "", a: "#" }
 const PRICE = { name: "", price: "", a: "" }
-
+const url = `https://fbecomm-8bab6-default-rtdb.firebaseio.com/dndsite.json`
 export default function CRUD() {
+    const state = useSelector(s => s)
+    const ref = useRef()
     const [ob, setob] = useState({
         header: { data: { logo: "", links: '' } },
         about: { data: '' },
@@ -25,11 +29,30 @@ export default function CRUD() {
 
     }
 
+    const create = () => {
+        let temp = {}
+        let c = ref?.current?.querySelectorAll("input")
+        c = Array.from(c)
+        c = c.map(x => ({ [x.placeholder]: x.value }))
+        // c=c.map(x=>({name:x.placeholder,val:x.value}))
+        // console.log("lkj", c)
+        for (let item of c) {
+            temp = { ...temp, ...item }
+        }
+        // console.log("lkj", temp)
+        // setob(temp)
+        let payload = {
+            uid: state?.user?.id,
+            data: JSON.stringify(temp)
+        }
+        axios.post(url, payload)
+    }
+
     return (
         <div className='crud'>
             <h1>CRUD-form</h1>
             {JSON.stringify(ob)}
-            <div>
+            <div ref={ref}>
                 <input onChange={handleChange} placeholder='header.data.logo' />
                 <input onChange={handleChange} placeholder='header.data.links' />
                 <br />
@@ -37,23 +60,24 @@ export default function CRUD() {
                 <input onChange={handleChange} placeholder='contact.data' />
                 <input onChange={handleChange} placeholder='services.data' />
                 {ob.team.data.map((x, i) => <div>
-                    <input onChange={handleChange} placeholder={`team.data.${i}.src`} />
-                    <input onChange={handleChange} placeholder={`team.data.${i}.h2`} />
-                    <input onChange={handleChange} placeholder={`team.data.${i}.h3`} />
-                    <input onChange={handleChange} placeholder={`team.data.${i}.h4`} />
-                    <input onChange={handleChange} placeholder={`team.data.${i}.a`} />
+                    <input onChange={handleChange} placeholder={`team.data[${i}].src`} />
+                    <input onChange={handleChange} placeholder={`team.data[${i}].h2`} />
+                    <input onChange={handleChange} placeholder={`team.data[${i}].h3`} />
+                    <input onChange={handleChange} placeholder={`team.data[${i}].h4`} />
+                    <input onChange={handleChange} placeholder={`team.data[${i}].a`} />
                     <button onClick={addmoreteam}>+</button>
                 </div>)}
                 {ob.price.data.map((x, i) => <div>
-                    <input onChange={handleChange} placeholder={`price.data.${i}.name`} />
-                    <input onChange={handleChange} placeholder={`price.data.${i}.price`} />
-                    <input onChange={handleChange} placeholder={`price.data.${i}.a`} />
+                    <input onChange={handleChange} placeholder={`price.data[${i}].name`} />
+                    <input onChange={handleChange} placeholder={`price.data[${i}].price`} />
+                    <input onChange={handleChange} placeholder={`price.data[${i}].a`} />
                     <button onClick={addmoreprice}>+</button>
                 </div>)}
 
                 <input onChange={handleChange} placeholder='map.data' />
                 <input onChange={handleChange} placeholder='footer.data' />
 
+                <button onClick={create}>create</button>
 
             </div>
         </div>
--- src/services/index.js
+++ src/services/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 const users = 'https://fbecomm-8bab6-default-rtdb.firebaseio.com/dndusers.json'
-const site = 'https://fbecomm-8bab6-default-rtdb.firebaseio.com/dndsite'
+const site = 'https://fbecomm-8bab6-default-rtdb.firebaseio.com/dndsite.json'
 
 
 const transform = ob => {
@@ -29,5 +29,30 @@ export const signupService = async (data) => {
     return resp
 }
 
+export const allSites = async () => {
+    let resp = await axios.get(site).then(d => transform(d.data)).catch(e => null)
+    return resp
+}
 
+export const mySite = async (userId) => {
+    let resp = await allSites()
+    if (!resp) return null
+    let myData = resp.find(x => x.uid === userId)
+    return myData
+}
 
+export const exists = async userId => {
+    let exists = await mySite(userId)
+    return !!exists
+}
+
+export const create = async (payload) => {
+    let { uid, data } = payload
+    let condition = await exists(uid)
+    if (condition) {
+        // patch
+    }
+    else {
+        // post
+    }
+}
